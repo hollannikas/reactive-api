25cf3ab86f1ac053893cfb1597d0c9a0362ad4e2
Rudolf Poels <rudolf.poels@futurice.com>
2018-06-28 10:16:19 +0300

    update repository and model
    Changed to a repository with template to have more control.
    Changed model into a data class, add generated createdDate
    and string id.

--- src/main/kotlin/fi/hollannikas/reactive/Model.kt
+++ src/main/kotlin/fi/hollannikas/reactive/Model.kt
@@ -1,19 +1,28 @@
 package fi.hollannikas.reactive
 
-import org.bson.types.ObjectId
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import java.time.LocalDateTime
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.data.mongodb.core.findAll
+import org.springframework.data.mongodb.core.findById
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.core.mapping.Field
+import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
+import java.time.LocalDateTime.now
 
 
-internal interface MessageRepository : ReactiveMongoRepository<MessageDocument, String>
+@Repository
+class MessageRepository(private val template: ReactiveMongoTemplate) {
+    fun save(message: Mono<Message>) = template.save(message)
+    fun findOne(id: String) = template.findById<Message>(id)
+    fun findAll() = template.findAll<Message>()
+}
 
 @Document
-internal class MessageDocument(
-        @Id val id: ObjectId,
+data class Message(
+        @Id val id: String? = null,
         @Field val text: String,
-        @CreatedDate val createdDate: LocalDateTime
+        @CreatedDate val createdDate: LocalDateTime = now()
 )
