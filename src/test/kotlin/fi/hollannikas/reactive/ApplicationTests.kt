f6a7a96752bfbfac1449e4bb04eba4d0d243365d
Rudolf Poels <rudolf.poels@futurice.com>
2018-06-28 10:18:27 +0300

    add springboot test

--- src/test/kotlin/fi/hollannikas/reactive/ApplicationTests.kt
+++ src/test/kotlin/fi/hollannikas/reactive/ApplicationTests.kt
@@ -3,9 +3,12 @@ package fi.hollannikas.reactive
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class ApplicationTests {
 
     @Test
