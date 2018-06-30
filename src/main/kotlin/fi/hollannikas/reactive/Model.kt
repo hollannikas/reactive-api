d4be589cff1ac79e361dfe32600b707cb90956de
Rudolf Poels <rudolf.poels@futurice.com>
2018-06-30 19:59:31 +0300

    use ReactiveCrudRepository and tailable finder
    Less boilerplate and makes it possible to stream data.

--- src/main/kotlin/fi/hollannikas/reactive/Model.kt
+++ src/main/kotlin/fi/hollannikas/reactive/Model.kt
@@ -1,26 +1,22 @@
 package fi.hollannikas.reactive
 
-import java.time.LocalDateTime
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.Id
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate
-import org.springframework.data.mongodb.core.findAll
-import org.springframework.data.mongodb.core.findById
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.core.mapping.Field
-import org.springframework.stereotype.Repository
-import reactor.core.publisher.Mono
+import org.springframework.data.mongodb.repository.Tailable
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import reactor.core.publisher.Flux
+import java.time.LocalDateTime
 import java.time.LocalDateTime.now
 
 
-@Repository
-class MessageRepository(private val template: ReactiveMongoTemplate) {
-    fun save(message: Mono<Message>) = template.save(message)
-    fun findOne(id: String) = template.findById<Message>(id)
-    fun findAll() = template.findAll<Message>()
+interface MessageRepository : ReactiveCrudRepository<Message, String> {
+    @Tailable
+    fun findWithTailableCursorBy(): Flux<Message>
 }
 
-@Document
+@Document(collection = "messages")
 data class Message(
         @Id val id: String? = null,
         @Field val text: String,
